dabd37281c61d55c0d3324270b4106e54158e51a
eunjung0402 <76139130+eunjung0402@users.noreply.github.com>
2020-12-17 14:21:24 +0900

    Add files via upload

---
+++ script/kbank.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.menuBtn').click(function(){
+        $('#menuBox').fadeIn();
+    });
+    
+    
+    $('.closeBtn').click(function(){
+        $('#menuBox').fadeOut();
+    });
+    
+});
---
+++ script/tabmenu.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.tabM a').click(function(){
+        $('.tabM a').removeClass('act');
+        $(this).addClass('act');
+        var i=$(this).index();
+        $('.tabC > div').hide();
+        $('.tabC > div').eq(i).show();
+        
+        return false;
+    });
+});
